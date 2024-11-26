47f830ee924a88db83c3cc8c163b53217ebc5550
Frozen Forest Reality Technologies <frozenforeststudio@gmail.com>
2024-11-27 00:12:01 +0300

    UE5.5 Update

--- Source/POCO/POCO.Build.cs
+++ Source/POCO/POCO.Build.cs
@@ -7,7 +7,7 @@ namespace UnrealBuildTool.Rules
         public POCO(ReadOnlyTargetRules Target) : base(Target)
         {
     		Type = ModuleType.External;
-            bEnableUndefinedIdentifierWarnings = false;
+            UndefinedIdentifierWarningLevel = WarningLevel.Off;
             bEnableExceptions = true;
             bUseRTTI = true;
 
